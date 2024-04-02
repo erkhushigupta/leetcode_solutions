9a1fa420fea3d146892b1764fb9a6d5cc3d8fb19
erkhushigupta <139675402+erkhushigupta@users.noreply.github.com>
2024-04-02 23:44:00 +0530

    Time: 43 ms (84.79%), Space: 16.8 MB (98.62%) - LeetHub

---
+++ 0893-groups-of-special-equivalent-strings/0893-groups-of-special-equivalent-strings.py
@@ -0,0 +1,5 @@
+class Solution:
+  def numSpecialEquivGroups(self, words: List[str]) -> int:
+    return len({''.join(sorted(word[::2])) + ''.join(sorted(word[1::2]))
+                for word in words})
+        
