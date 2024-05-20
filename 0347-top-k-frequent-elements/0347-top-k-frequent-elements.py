322512946b958ab56c5175b43e976d900997cc10
erkhushigupta <139675402+erkhushigupta@users.noreply.github.com>
2024-05-20 22:03:05 +0530

    Time: 83 ms (84.51%), Space: 21.1 MB (73.24%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,48 @@
+from collections import Counter
+
+from heapq import heappush, heappop
+
+
+class Solution:
+
+    def topKFrequent(self, nums, k):
+
+        # Count the frequency of each number in nums using Counter.
+
+        num_frequencies = Counter(nums)
+
+      
+
+        # Initialize a min heap to keep track of top k elements.
+
+        min_heap = []
+
+      
+
+        # Iterate over the number-frequency pairs.
+
+        for num, freq in num_frequencies.items():
+
+            # Push a tuple of (frequency, number) onto the heap.
+
+            # Python's heapq module creates a min-heap by default.
+
+            heappush(min_heap, (freq, num))
+
+          
+
+            # If the heap size exceeds k, remove the smallest frequency element.
+
+            if len(min_heap) > k:
+
+                heappop(min_heap)
+
+      
+
+        # Extract the top k frequent numbers by taking the second element of each tuple.
+
+        top_k_frequent = [pair[1] for pair in min_heap]
+
+      
+
+        return top_k_frequent
